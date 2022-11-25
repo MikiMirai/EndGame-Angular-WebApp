f24ba1ff10b7322b5c557bd8d5ae6acff7810b46
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2022-11-25 13:23:59 +0200

    Init commit.

---
+++ src/app/games-list/games-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games-list',
+  templateUrl: './games-list.component.html',
+  styleUrls: ['./games-list.component.scss']
+})
+export class GamesListComponent {
+
+}
