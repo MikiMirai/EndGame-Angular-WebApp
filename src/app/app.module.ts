a3363f220c4cf60d753bb58d353795b3e129c16e
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2022-11-30 16:58:34 +0200

    Added Contact Page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { GamesListComponent } from './games-list/games-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { GameDetailsComponent } from './game-details/game-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GamesListComponent
+    GamesListComponent,
+    ContactComponent,
+    GameDetailsComponent,
   ],
   imports: [
     BrowserModule
