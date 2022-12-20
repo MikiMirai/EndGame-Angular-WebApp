eaa0dea8d7fde29f4ee11052bdd8b6f4cdc50db9
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2022-12-20 14:59:26 +0200

    Fixed some routing and visual bugs.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { ContactComponent } from "./contact/contact.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { GameDetailsComponent } from "./game-details/game-details.component";
 import { GamesListComponent } from "./games-list/games-list.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./register/register.component";
@@ -13,7 +14,8 @@ const routes: Routes = [
         pathMatch: 'full',
         redirectTo: '/'
     },
-    { path: 'games-list', component: GamesListComponent },
+    { path: 'games', component: GamesListComponent },
+    { path: 'game-details', component: GameDetailsComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
