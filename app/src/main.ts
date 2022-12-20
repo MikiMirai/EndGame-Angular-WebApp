ff6c3377ed7fe94a4d2e98ef15d6250b9a4d149f
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2022-12-20 18:59:25 +0200

    Re-upload after folder move.

---
+++ app/src/main.ts
@@ -0,0 +1,12 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { environment } from './environments/environment';
+
+import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
