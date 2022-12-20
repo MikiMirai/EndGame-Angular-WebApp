8d1ea7737d6630e73b45e528d242ef0b41d9a01d
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2022-12-20 14:58:32 +0200

    Removed some not needed stuff.

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import { AppModule } from './app/app.module';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, set, update, onValue, DatabaseReference, get, child } from "firebase/database";
-import { Game } from './app/shared/services/game';
-import { DataSnapshot } from '@angular/fire/compat/database/interfaces';
-import * as firebase from 'firebase/compat';
+import { getDatabase, ref, set, onValue } from "firebase/database";
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
@@ -33,23 +30,22 @@ function writeUserData(userId: string, name: string, email: string, password: st
     });
 }
 
-function writeGameData(gameId: string, name: string, picture: string, description: string, genres: string, rating: string) {
+function writeGameData(gameId: string, title: string, picture: string, description: string, genres: string, rating: string) {
   const db = getDatabase();
   const reference = ref(db, 'games/' + gameId);
 
   set(reference,
     {
-      name: name,
+      title: title,
       picture: picture,
       description: description,
       genres: genres,
       rating: rating
     });
-
 }
 
-//writeGameData("firstGame","God of War", "https://cdn.cloudflare.steamstatic.com/steam/apps/1593500/capsule_616x353.jpg?t=1650554420", "Great Game(description)", "Open world, Adventure", "10")
-// writeUserData("MikuId", "Miki", "Miki2000@gmail.com", "password123")
+//writeGameData("firstGame", "God of War", "https://cdn.cloudflare.steamstatic.com/steam/apps/1593500/capsule_616x353.jpg?t=1650554420", "Great Game(description)", "Open world, Adventure", "10");
+// writeUserData("MikuId", "Miki", "Miki2000@gmail.com", "password123");
 
 function readUser(userId: string) {
   const db = getDatabase();
@@ -60,30 +56,3 @@ function readUser(userId: string) {
 }
 
 //console.log(readUser("MikuId"));
-let gameData = new Game;
-
-function updateVal(name: string, val: string){
-  if (Object.hasOwn(gameData, name)) {
-    Object.defineProperty(gameData, name, val)
-  }
-}
-
-function ReadGameData(gameId: string) {
-  const db = getDatabase();
-  const gameRef = ref(db, 'games/' + gameId);
-  onValue(gameRef, (snapshot) => {
-    const data = snapshot.val();
-    updateVal('name', `${data.name}`)
-    // gameData.name = data.name;
-    // gameData.picture = data.picture;
-    // gameData.description = data.description;
-    // gameData.genres = data.genres;
-    // gameData.rating = data.rating;
-  });
-};
-
-ReadGameData("firstGame");
-
-let gameName = gameData.name;
-
-console.log(gameName);
