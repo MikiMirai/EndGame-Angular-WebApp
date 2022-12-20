7c6ff2669e5b2cefc150b231e80e10df2d1c7b7d
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2022-12-20 22:22:48 +0200

    Load test if any.

---
+++ app/src/test.ts
@@ -0,0 +1,23 @@
+// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+declare const require: {
+  context(path: string, deep?: boolean, filter?: RegExp): {
+    <T>(id: string): T;
+    keys(): string[];
+  };
+};
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
+
+const context = require.context('./', true, /\.spec\.ts$/);
+context.keys().forEach(context);
