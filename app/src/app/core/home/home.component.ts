b539da63caba92d7b599288f6ac4e153b353961d
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2022-12-21 19:36:34 +0200

    Dynamics added.

--- app/src/app/core/home/home.component.ts
+++ app/src/app/core/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AuthService } from 'src/app/auth/auth.service';
+import { ApiService } from 'src/app/api.service';
+import { IGame } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,32 @@ import { AuthService } from 'src/app/auth/auth.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  gameListRecent: IGame[] | null = null;
+  gameListTrending: IGame[] | null = null;
 
+  errorFetcingData = false;
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getGamesRecent().subscribe({
+      next: (value) => {
+        this.gameListRecent = value.reverse().slice(0, 3);
+      },
+      error: (err) => {
+        this.errorFetcingData = true;
+        console.error(err);
+      }
+    });
+
+    this.apiService.getGamesRecent().subscribe({
+      next: (value) => {
+        this.gameListTrending = value.sort((a, b) => +b.rating - +a.rating).slice(0, 3);
+      },
+      error: (err) => {
+        this.errorFetcingData = true;
+        console.error(err);
+      }
+    });
+  }
 }
