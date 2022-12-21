9dc80424f85b6f99c61e05c53ca05576ea4bae12
MikiMirai <100376192+MikiMirai@users.noreply.github.com>
2022-12-21 19:36:24 +0200

    Links and styling fix.

--- app/src/app/core/footer/footer.component.ts
+++ app/src/app/core/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  get isLoggedIn() {
+    return this.authService.isLoggedIn;
+  }
 
+  constructor(private authService: AuthService, private router: Router) { }
+  
   ngOnInit(): void {
   }
 
